5ae1f659010152dd60b280a03c16a188c5a5ae10
srappltd <abhaygautam626039@gmail.com>
2024-03-01 01:33:01 +0530

    first commit

---
+++ routes/users.js
@@ -0,0 +1,52 @@
+const mongoose = require("mongoose");
+const plm = require("passport-local-mongoose");
+mongoose.connect(`mongodb://127.0.0.1:27017/instacloney`);
+
+const userSchema = mongoose.Schema({
+  username:{type:String,require:true},
+  name: {type:String,require:true},
+  stories: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "story"
+  }],
+  followers: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "user",
+    },
+  ],
+  followings: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "user",
+    },
+  ],
+  posts: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "post",
+    },
+  ],
+  story: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "story",
+    },
+  ],
+  messages: {
+    type: Array,
+    default: [],
+  },
+  profilepicture: {
+    type: String,
+    default: "default.jpg",
+  },
+  bio: String,
+  password: String,
+  email:{type:String,require:true},
+  saved: [{ type: mongoose.Schema.Types.ObjectId, ref: "post" }],
+});
+
+mongoose.plugin(plm);
+
+module.exports = mongoose.model("user", userSchema);
